bc9e1df1b4f2807201703e246d418d2fadef9ed1
nielsbakkers <niels.bakkers@gmail.com>
2023-02-02 14:08:27 +0100

    added mobile page

--- script.js
+++ script.js
@@ -1,36 +1,9 @@
-const wrapper = document.getElementById("wrapper");
-
-const rand = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
-
-const uniqueRand = (min, max, prev) => {
-  let next = prev;
-  
-  while(prev === next) next = rand(min, max);
-  
-  return next;
+//CHECK IF THE VISITOR IS USING A MOBILE DEVICE
+if (screen.width <= 699) {
+    document.location = "mobile.html";
 }
 
-const combinations = [
-  { configuration: 1, roundness: 1 },
-  { configuration: 1, roundness: 2 },
-  { configuration: 1, roundness: 4 },
-  { configuration: 2, roundness: 2 },
-  { configuration: 2, roundness: 3 },
-  { configuration: 3, roundness: 3 }
-];
-
-let prev = 0;
-
-setInterval(() => {
-  const index = uniqueRand(0, combinations.length - 1, prev),
-        combination = combinations[index];
-  
-  wrapper.dataset.configuration = combination.configuration;
-  wrapper.dataset.roundness = combination.roundness;
-  
-  prev = index;
-}, 3000);
-
+//PAGE SCROLL EFFECT
 $(document).ready(function() {
   var delay = false;
   var currentPage = 1;
@@ -69,6 +42,38 @@ $(document).ready(function() {
     });
 });  
 
+//SECTION 1 - SHAPE ANIMATIONS
+const wrapper = document.getElementById("wrapper");
+const rand = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+const uniqueRand = (min, max, prev) => {
+  let next = prev;
+  
+  while(prev === next) next = rand(min, max);
+    return next;
+}
+
+const combinations = [
+  { configuration: 1, roundness: 1 },
+  { configuration: 1, roundness: 2 },
+  { configuration: 1, roundness: 4 },
+  { configuration: 2, roundness: 2 },
+  { configuration: 2, roundness: 3 },
+  { configuration: 3, roundness: 3 }
+];
+
+let prev = 0;
+setInterval(() => {
+  const index = uniqueRand(0, combinations.length - 1, prev),
+        combination = combinations[index];
+ 
+  wrapper.dataset.configuration = combination.configuration;
+  wrapper.dataset.roundness = combination.roundness;
+  
+  prev = index;
+}, 3000);
+
+
+//SECTION TWO - word animation
 const letters = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
 document.querySelector("h1").onmouseover = event => {
     let interations = 0;
