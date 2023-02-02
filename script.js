616c67040c84b53c469735ec23f6e0c6e6562346
nielsbakkers <niels.bakkers@gmail.com>
2023-02-02 10:19:23 +0100

    added section 2

--- script.js
+++ script.js
@@ -68,3 +68,24 @@ $(document).ready(function() {
         }
     });
 });  
+
+const letters = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
+document.querySelector("h1").onmouseover = event => {
+    let interations = 0;
+
+    const interval = setInterval(() => {
+        event.target.innerText = event.target.innerText.split("")
+            .map((letter, index) => {
+                if(index < interations) {
+                    return event.target.dataset.value[index];
+                }
+
+                return letters[Math.floor(Math.random() * 26)]
+            })
+            .join("");
+
+        if(interations >= event.target.dataset.value.length) clearInterval(interval);
+
+        interations += 1 / 3;
+    }, 30);
+}
